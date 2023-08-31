405846fb37583c48ec45eb384163981aabf1edb3
Mark Prangan <pranganmark@gmail.com>
2023-08-31 11:36:27 +1000

    Added button that changes the read status of the book on screen and in the book object

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Library
+
+Created a simple library website where you would add/remove books from the library. This project was created as my solution to
+The Odin Project's Full Stack Library project. Later when I progress through the course or when I have the time, I hope to add some kind of storage to store a person's library. This would allow the user to be able to come back any time and still have their data the same - without the need of having to enter it all in again. 
+
+Overall, I found this project somewhat easy. There were some parts where I either had to think really hard or search up the solution on Google. Overall, I am really happy with the way it turned out.
--- app.js
+++ app.js
@@ -34,21 +34,49 @@ function makeBookDiv(arg) {
     book.setAttribute('style', 'white-space: pre;');
     book.dataset.index = myLibrary.length - 1;
     body.appendChild(book);
+
     const bookInfo = document.createElement('div');
     bookInfo.classList.add('bookInfo');
-    book.appendChild(bookInfo)
-    bookInfo.textContent = `Title: ${arg.title}\r\nAuthor: ${arg.author}\r\nNumber of Pages: ${arg.pages}\r\nRead: ${arg.read}`;
+    book.appendChild(bookInfo);
+
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+    const status = document.createElement('div');
+    title.textContent = `Title: ${arg.title}`;
+    author.textContent = `Author: ${arg.author}`;
+    pages.textContent = `Pages: ${arg.pages}`;
+    status.textContent = `Read: ${arg.read}`;
+    bookInfo.appendChild(title);
+    bookInfo.appendChild(author);
+    bookInfo.appendChild(pages);
+    bookInfo.appendChild(status);
+
     const btnDiv = document.createElement('div');
     btnDiv.classList.add('btnDiv');
     book.appendChild(btnDiv);
     const remove = document.createElement('button');
     remove.classList.add('remove');
-    remove.innerText = "- remove book";
+    remove.innerText = "remove book";
     remove.addEventListener('click', () => {
         book.remove();
-        myLibrary.splice(book.dataset.index, 1)
+        myLibrary.splice(book.dataset.index, 1);
     })
     btnDiv.appendChild(remove);
+
+    const statusChange = document.createElement('button');
+    statusChange.classList.add('change');
+    statusChange.innerText = "change read status";
+    statusChange.addEventListener('click', () => {
+        if (status.textContent == "Read: Yes") {
+            status.textContent = "Read: No";
+            myLibrary[book.dataset.index].read = "No"
+        } else {
+            status.textContent = "Read: Yes";
+            myLibrary[book.dataset.index].read = "Yes"
+        }
+    })
+    btnDiv.appendChild(statusChange);
 }
 
 submitBtn.addEventListener('click', (event) => {
@@ -58,5 +86,3 @@ submitBtn.addEventListener('click', (event) => {
         form.reset()
     }
 })
-
-displayBook(myLibrary);
